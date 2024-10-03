caead3278763eaa8cb286b9aeaf168385147395e
Thomas <thomas.rerolle@aeveil.ch>
2024-10-03 17:13:02 +0200

    king of working with objects

--- yaml_macro_proc/src/lib.rs
+++ yaml_macro_proc/src/lib.rs
@@ -154,7 +154,7 @@ fn generate_struct(
                         let nested_struct_name =
                             format_ident!("{}{}", parent_name, k.as_str().unwrap());
 
-                        let (nested_struct, struct_value) =
+                        let (nested_struct, nested_nested_struct) =
                             generate_struct(&nested_struct_name.to_string(), v);
 
                         nested_structs.push(quote! {
@@ -164,15 +164,53 @@ fn generate_struct(
                             }
                         });
 
-                        fields.push(quote! {
-                            pub: #key: #nested_struct_name
-                        });
+                        nested_structs.extend(nested_nested_struct);
+                        // fields.push(quote! {
+                        //     pub #key: #nested_struct_name,
+                        // });
 
                         quote! { #nested_struct_name}
                     }
+                    // JAMA: c'est ici
+                    // Value::Sequence(seq) => {
+                    //     // Déterminer le type des éléments dans la séquence
+                    //     if let Some(first_value) = seq.first() {
+                    //         match first_value {
+                    //             Value::String(_) => quote! { Vec<String> },
+                    //             Value::Number(_) => quote! { Vec<i32> },
+                    //             Value::Bool(_) => quote! { Vec<bool> },
+                    //             Value::Mapping(_) => {
+                    //                 // Si la séquence contient des structures imbriquées
+                    //                 let nested_struct_name =
+                    //                     format_ident!("{}{}Item", parent_name, k.as_str().unwrap());
+
+                    //                 // Générer la sous-structure pour les éléments de la séquence
+                    //                 let (nested_fields, nested_nested_structs) = generate_struct(
+                    //                     &nested_struct_name.to_string(),
+                    //                     first_value,
+                    //                 );
+
+                    //                 nested_structs.push(quote! {
+                    //                     #[derive(Debug, serde::Deserialize)]
+                    //                     pub struct #nested_struct_name {
+                    //                         #(#nested_fields)*
+                    //                     }
+                    //                 });
+
+                    //                 // Ajouter les sous-structures imbriquées
+                    //                 nested_structs.extend(nested_nested_structs);
+
+                    //                 quote! { Vec<#nested_struct_name> }
+                    //             }
+                    //             _ => quote! { Vec<serde_yaml::Value> }, // Si le type est inconnu ou mixte
+                    //         }
+                    //     } else {
+                    //         // Si la séquence est vide, utiliser un type générique
+                    //         quote! { Vec<serde_yaml::Value> }
+                    //     }
+                    // }
                     _ => quote! { serde_yaml::Value }, // Pour les types non pris en charge
                 };
-
                 fields.push(quote! {
                     pub #key: #value_type,
                 });
